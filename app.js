4ebae51ecd56fdab7b0cf87ea92743cd6dab375e
Salem <salemmosbahi@gmail.com>
2016-04-05 18:16:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+var port       = process.env.PORT || 7007;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./route/routes.js')(app);
+
+app.listen(port);
+console.log('The App Shared Services runs at port: ' + port);
---
+++ route/routes.js
@@ -0,0 +1,50 @@
+var encryption	= require('./encryption');
+var country = require('controllers/country');
+var user	= require('controllers/user');
+
+module.exports = function(app){
+
+  app.get('/', function(req, res) {
+		res.end('Shared Services Project');
+	});
+
+  app.post('/getAllCountry',function(req,res){
+		country.getAllCountry(function(found){
+			res.json(found);
+		});
+	});
+
+	app.post('/getAllCity',function(req,res){
+		var name = req.body.name;
+		country.getAllCity(name,function(found){
+			res.json(found);
+		});
+	});
+
+	app.post('/signup',function(req,res){
+		var key = encryption.key(req.body.key);
+    var picture = req.body.picture;
+		var fname = encryption.decode(req.body.fname,key);
+		var lname = encryption.decode(req.body.lname,key);
+		var gender = encryption.decode(req.body.gender,key);
+		var dateN = encryption.decode(req.body.dateN,key);
+		var country = encryption.decode(req.body.country,key);
+		var city = encryption.decode(req.body.city,key);
+		var email = encryption.decode(req.body.email,key);
+		var password = encryption.decode(req.body.password,key);
+		var phone = encryption.decode(req.body.phone,key);
+    var driver = (encryption.decode(req.body.driver,key) == "true") ? true : false;
+    user.signup(picture,fname,lname,gender,dateN,country,city,email,password,phone,driver,function(found){
+      res.json(found);
+    });
+	});
+
+	app.post('/login',function(req,res){
+		var key = encryption.key(req.body.key);
+		var email = encryption.decode(req.body.email,key);
+		var password = encryption.decode(req.body.password,key);
+    user.login(email,password,function(found){
+      res.json(found);
+    });
+	});
+}
