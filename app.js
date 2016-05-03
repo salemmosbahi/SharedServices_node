2894b066e96f6990002e58edb38b02369bbd5995
Salem <salemmosbahi@gmail.com>
2016-05-03 20:33:37 +0100

    error request server v2

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var http       = require('http').Server(app);
 var io         = require('socket.io')(http);
 var port       = process.env.PORT || 4000;
 
-//app.use(bodyParser.urlencoded({limit:'50mb', extended:true}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({limit:'5000mb', extended:true}));
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 require('./route/routes.js')(app);
